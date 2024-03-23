e5ec98a1becd5cfaf1a9f3c72809f0f935c57c2d
Samet <erkan.samet@ogr.deu.edu.tr>
2024-03-23 14:58:24 +0300

    Create Form1.cs

---
+++ Exercise 12.6. Procedure ve Metot Hazırlamak/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_12._6.Procedure_ve_Metot_Hazırlamak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void böl()
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            if(b == 0)
+            {
+                textBox3.Text = "Bölüm Belirsiz!";
+                return;
+            }
+            textBox3.Text = (a / b).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            böl();
+        }
+    }
+}
